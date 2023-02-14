b6b08309d3d97eb8a9c015db261dacb6bdf16d40
Bobby Hinton <bob.g.hint@gmail.com>
2023-02-13 22:32:28 -0600

    commit tree 3aedb6a5b6b29da6a1a1eae49efc0c7a9d99f93c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  belongs_to :author
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+
+  # post.author = Author.new(... ) will not work as post.author will return nil
+  # until it is defined
+
+  # as such, post.author.create would throw an error
+
+  # instead we must use post = Post.new(.....)
+
+  # new_author = post.create_author(.... )
+
+  #  we can also use build_ if we want to create a new instance but not 
+  # persist it to the db 
 end
